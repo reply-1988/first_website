227cada34a710aa3d50d66fd42d298d82e6d4726
reply-1988 <l>
2017-08-18 16:30:02 +0800

    2.1

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post, Category, Tag
+import markdown
 # Create your views here.
 
 
@@ -12,6 +13,12 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'mysite/detail.html',
                   context={'post': post})
 
