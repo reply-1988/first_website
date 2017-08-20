db52781c38b2df9c0865c21ed6751fcef78f41b7
reply-1988 <l>
2017-08-20 15:05:56 +0800

    4 类视图处理

--- mysite/models.py
+++ mysite/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.utils.html import strip_tags
+import markdown
 
 import mysite
 
@@ -28,9 +30,26 @@ class Post(models.Model):
     category = models.ForeignKey(Category)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('mysite:detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:50]
+        super(Post, self).save(*args, **kwargs)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    class Meta:
+        ordering = ['created_time']
